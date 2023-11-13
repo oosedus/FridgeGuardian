e33b64e041bec171851ba714cc011a2a68fbb353
oosedus <seayeon0729@gmail.com>
2023-11-13 18:29:16 +0900

    feat: Activity 생성 및 Navigation 생성

--- app/src/main/java/com/example/fridgeguardian/HomeActivity.kt
+++ app/src/main/java/com/example/fridgeguardian/HomeActivity.kt
@@ -1,4 +1,44 @@
 package com.example.fridgeguardian
 
-class HomeActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "Hello,\nCheck your expiration date"
+        setSupportActionBar(toolbar)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.navigation)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    true
+                }
+                R.id.nav_recipe -> {
+                    val intent = Intent(this, RecipeActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_community -> {
+                    val intent = Intent(this, CommunityActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_mypage -> {
+                    val intent = Intent(this, MyPageActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 }
+
