69c25b899458667f77080c3ffcb3883d7274871c
oosedus <seayeon0729@gmail.com>
2023-11-13 16:46:00 +0900

    feat: 회원가입 및 로그인 기능 구현

---
+++ app/src/main/java/com/example/fridgeguardian/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.fridgeguardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        // 회원가입 기능
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.email)
+            val password = findViewById<EditText>(R.id.password)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+
+                    } else {
+                        Toast.makeText(this, "실패 ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                    }
+
+
+                }
+
+        }
+
+        // 로그인 기능
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.email)
+            val password = findViewById<EditText>(R.id.password)
+
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+        // 로그아웃 기능
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+        logoutBtn.setOnClickListener {
+            Firebase.auth.signOut()
+            Toast.makeText(this, "로그아웃", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
