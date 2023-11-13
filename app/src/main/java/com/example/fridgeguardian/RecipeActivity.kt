c48e65d0c35f1f2ddf0aa7b062488e02b7374a96
junhyukkkk <cjunk0304@gmail.com>
2023-11-13 22:58:56 +0900

    First Recipe Page

--- app/src/main/java/com/example/fridgeguardian/RecipeActivity.kt
+++ app/src/main/java/com/example/fridgeguardian/RecipeActivity.kt
@@ -1,4 +1,48 @@
 package com.example.fridgeguardian
 
-class RecipeActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class RecipeActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipe_activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "Recipe"
+        setSupportActionBar(toolbar)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.navigation)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_recipe -> {
+                    true
+                }
+                R.id.nav_community -> {
+                    val intent = Intent(this, CommunityActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_mypage -> {
+                    val intent = Intent(this, MyPageActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
+
+
+    }
 }
